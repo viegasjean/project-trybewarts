ed9be92245cc215fdf8e48e5a829c97ea0d18036
Jean Viegas <jeanmelg@gmail.com>
2021-11-09 15:37:16 -0300

    feito ate o requisito 14

---
+++ script.js
@@ -0,0 +1,24 @@
+const email = document.getElementsByName('email');
+const password = document.getElementsByName('password');
+const btEnter = document.getElementById('enter');
+btEnter.addEventListener('click', () => {
+  if (email[0].value === 'tryber@teste.com' && password[0].value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
+
+const evaluationForm = document.getElementById('rates');
+
+for (let i = 1; i <= 10; i += 1) {
+  const radio = document.createElement('input');
+  radio.type = 'radio';
+  radio.value = i;
+  radio.name = 'rate';
+  const label = document.createElement('label');
+  label.htmlFor = 'rate';
+  label.innerText = i;
+  evaluationForm.appendChild(label);
+  evaluationForm.appendChild(radio);
+}
